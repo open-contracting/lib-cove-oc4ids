7c91faed17d150661eecad399ddfccad668c60f6
Michael Wood <michael.wood@opendataservices.coop>
2019-06-24 21:27:18 +0100

    libcoveoc4ids config Remove id_name config item
    This config item isn't used to remove it to avoid confusion

--- libcoveoc4ids/config.py
+++ libcoveoc4ids/config.py
@@ -8,7 +8,6 @@ LIB_COVE_OC4IDS_CONFIG_DEFAULT.update({
     # These default values are very wide on purpose. It is left to apps using this to tighten them up.
     'root_list_path': 'projects',
     'root_id': 'id',
-    'id_name': 'id',
     'root_is_list': False,
     'convert_titles': False,
     'schema_name': 'project-package-schema.json',
