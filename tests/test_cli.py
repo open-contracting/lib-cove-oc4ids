5d8ee4b52b5334dd532760a2504193e2c14aa3ff
Michael Wood <michael.wood@opendataservices.coop>
2019-07-01 11:25:34 +0100

    tests Add basic sanity check for cli tool
    Checks that the cli tools runs as expected. The output and validation
    paths are covered by the api tests.

---
+++ tests/test_cli.py
@@ -0,0 +1,16 @@
+import subprocess
+import tests.utils as utils
+import json
+
+
+def test_cli_runs():
+    """ Test the cli tool runs as expected """
+    fixture_path = utils.get_path_for_fixture("example-data.json")
+
+    # If non-zeo exit status this will raise CalledProcessError
+    output = subprocess.check_output(['coveoc4ids-tool', fixture_path])
+
+    # Decode to str from bytes for older python (3.5) which doesn't support
+    # byte input to loads.
+    # output json should be parseable
+    json.loads(output.decode("utf-8"))
