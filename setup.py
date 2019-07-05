806a654e4b1a7866b6acc65e58db89f5e0c5b983
Michael Wood <michael.wood@opendataservices.coop>
2019-07-05 17:14:04 +0100

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='libcoveoc4ids',
-    version='0.1.0',
+    version='0.1.1',
     author='Open Data Services',
     author_email='code@opendataservices.coop',
     url='https://github.com/opendataservices/lib-cove-oc4ids',
