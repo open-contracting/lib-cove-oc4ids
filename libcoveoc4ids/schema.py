6e5e94434b265f261b839ab449b8b5ec3bf903ed
Michael Wood <michael.wood@opendataservices.coop>
2019-07-08 17:33:02 +0100

    config Add schema version information
    cove-oc4ids Issue #10 .1 .2

--- libcoveoc4ids/schema.py
+++ libcoveoc4ids/schema.py
@@ -5,6 +5,10 @@ from urllib.parse import urljoin
 class SchemaOC4IDS(SchemaJsonMixin):
 
     def __init__(self, lib_cove_oc4ids_config=None):
+        # Currenly this mostly copies config into the object as properties
+        self.version = lib_cove_oc4ids_config.config['schema_version']
+        self.version_choices = \
+            lib_cove_oc4ids_config.config['schema_version_choices']
         self.schema_host = lib_cove_oc4ids_config.config['schema_host']
         self.release_schema_name = lib_cove_oc4ids_config.config['schema_item_name']
         self.release_pkg_schema_name = lib_cove_oc4ids_config.config['schema_name']
