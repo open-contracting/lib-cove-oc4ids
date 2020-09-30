87fbd7191c12384235f3c6fd7378f4f798d67ce4
Michael Wood <michael.wood@opendataservices.coop>
2020-09-30 16:57:54 +0100

    schema: Fix missing 'config' property in SchemaOC4IDS
    This is now required to exist by libcove since >=0.19.0
    
    Fixes https://github.com/open-contracting/lib-cove-oc4ids/issues/20

--- libcoveoc4ids/schema.py
+++ libcoveoc4ids/schema.py
@@ -1,17 +1,19 @@
 from urllib.parse import urljoin
 
 from libcove.lib.common import SchemaJsonMixin
+import libcoveoc4ids.config
 
 
 class SchemaOC4IDS(SchemaJsonMixin):
-
     def __init__(self, lib_cove_oc4ids_config=None):
-        # Currenly this mostly copies config into the object as properties
-        self.version = lib_cove_oc4ids_config.config['schema_version']
-        self.version_choices = \
-            lib_cove_oc4ids_config.config['schema_version_choices']
-        self.schema_host = lib_cove_oc4ids_config.config['schema_host']
-        self.schema_name = lib_cove_oc4ids_config.config['schema_item_name']
-        self.pkg_schema_name = lib_cove_oc4ids_config.config['schema_name']
+        self.config = (
+            lib_cove_oc4ids_config or libcoveoc4ids.config.LibCoveOC4IDSConfig()
+        )
+
+        self.version = self.config.config["schema_version"]
+        self.version_choices = self.config.config["schema_version_choices"]
+        self.schema_host = self.config.config["schema_host"]
+        self.schema_name = self.config.config["schema_item_name"]
+        self.pkg_schema_name = self.config.config["schema_name"]
         self.schema_url = urljoin(self.schema_host, self.schema_name)
         self.pkg_schema_url = urljoin(self.schema_host, self.pkg_schema_name)
