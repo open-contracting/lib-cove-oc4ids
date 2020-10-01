1b409982ce7b62a1aece639409a66de720e9e8c5
Michael Wood <michael.wood@opendataservices.coop>
2020-10-01 19:26:38 +0100

    setup: Add Django which is used by libcov
    This forces the django version correctly to influence the dependencies
    further down the chain. It looks like "Django" being unspecified in
    libcoveocds could be the root of this.

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     packages=find_packages(exclude=['tests', 'tests.*']),
     long_description=long_description,
     install_requires=[
+        'Django<2.3',
         'flattentool',
         'libcoveocds>=0.8.0',
         'libcove>=0.18.0',
