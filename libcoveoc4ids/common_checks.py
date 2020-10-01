b546e1449cbd8f1c072f7b721ddd5cbd276f4459
Michael Wood <michael.wood@opendataservices.coop>
2020-10-01 18:29:33 +0100

    common_checks: Add additional_checks with EmptyFieldCheck
    Use EmptyFieldCheck from libcoveocds.
    
    Fixes: https://github.com/open-contracting/cove-oc4ids/issues/45

--- libcoveoc4ids/common_checks.py
+++ libcoveoc4ids/common_checks.py
@@ -1,10 +1,11 @@
 from libcove.lib.common import common_checks_context
+from libcoveocds.lib.additional_checks import EmptyFieldCheck
+from libcoveocds.lib.additional_checks import run_additional_checks as libcoveocds_run_additional_checks
 
 from libcoveoc4ids.config import LibCoveOC4IDSConfig
 
 
-def common_checks_oc4ids(context, upload_dir, json_data, schema_obj,
-                         lib_cove_oc4ids_config=None, cache=True):
+def common_checks_oc4ids(context, upload_dir, json_data, schema_obj, lib_cove_oc4ids_config=None, cache=True):
 
     if not lib_cove_oc4ids_config:
         lib_cove_oc4ids_config = LibCoveOC4IDSConfig()
@@ -12,6 +13,12 @@ def common_checks_oc4ids(context, upload_dir, json_data, schema_obj,
     common_checks = common_checks_context(upload_dir, json_data, schema_obj,
                                           'schema.json', context, cache=cache)
 
-    context.update(common_checks['context'])
+    context.update(common_checks["context"])
+
+    additional_checks = libcoveocds_run_additional_checks(
+        json_data, [EmptyFieldCheck], ignore_errors=True, return_on_error=None
+    )
+
+    context.update({"additional_checks": additional_checks})
 
     return context
