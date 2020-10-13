fcfa31839f5a050d08c657fafaa79a6a18a77ab8
Michael Wood <michael.wood@opendataservices.coop>
2020-10-13 16:38:28 +0100

    tests: Add a tool for generating large test data
    example: libcoveoc4ids/tests$ generate_test_data.py 300

---
+++ tests/generate_test_data.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import argparse
+import copy
+import json
+import random
+
+
+def random_project_id():
+    alphanum = "abcdefghijklmnopqrstuvwxyz123456789"
+    random_range = (0, len(alphanum) - 1)
+
+    return "oc4ids-{}{}{}{}{}{}-test".format(
+        alphanum[random.randint(*random_range)],
+        alphanum[random.randint(*random_range)],
+        alphanum[random.randint(*random_range)],
+        alphanum[random.randint(*random_range)],
+        alphanum[random.randint(*random_range)],
+        alphanum[random.randint(*random_range)],
+    )
+
+
+def generate_data(num_projects):
+
+    with open("fixtures/example-additional-checks.json") as fp:
+        data = json.load(fp)
+
+        # Use first project as a "template"
+        project_template = data["projects"][0]
+
+        i = 0
+        while i != num_projects:
+            project = copy.deepcopy(project_template)
+            project["id"] = random_project_id()
+            data["projects"].append(project)
+            i += 1
+
+        with open("example-generated-data-{}-projects.json".format(num_projects), "w") as out_fp:
+            json.dump(data, out_fp)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(type=int, action="store", dest="num_projects", help="The number of projects to generate")
+
+    args = parser.parse_args()
+
+    generate_data(args.num_projects)
