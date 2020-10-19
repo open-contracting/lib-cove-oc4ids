19716dff483a78ab50a3e7f75c71e1ec6b8708a7
Michael Wood <michael.wood@opendataservices.coop>
2020-10-19 17:22:58 +0100

    conformance_checks: Move check for project id prefix to conformance
    Check for project prefix should have been the conformance class of check
    rather than an additional check. This refactors those two types into
    distinct types.

--- libcoveoc4ids/additional_checks.py
+++ libcoveoc4ids/additional_checks.py
@@ -1,7 +1,7 @@
 import gettext
 import re
 
-from libcoveoc4ids.additional_check import AdditionalCheck
+from libcoveoc4ids.check_classes import AdditionalCheck
 
 _ = gettext.gettext
 
@@ -56,37 +56,6 @@ class EmptyValueCheck(AdditionalCheck):
                 "Fields that are not being used, or that have no value, "
                 "should be excluded in their entirety (key and value) from the data"),
             missing,
-        )
-
-
-class ProjectPrefixCheck(AdditionalCheck):
-    def process(self, data, flat_data):
-
-        # matches /projects/10/id
-        project_path = re.compile(r".*\/projects\/[0-9]+\/id$")
-        # matches oc4ids-abc123-anything
-        valid_project_id = re.compile(r"oc4ids\-[a-z0-9]{6}.+$")
-
-        invalid_project_id_paths = []
-
-        for path, value in flat_data.items():
-            if project_path.match(path):
-                # Note if there is no value the regex can't match we leave it to
-                # the other checks to expose this error.
-                if value and valid_project_id.match(value) is None:
-                    invalid_project_id_paths.append(path)
-
-        if len(invalid_project_id_paths) == 0:
-            return True
-
-        return self.result(
-            "invalid-project-ids",
-            _(
-                "%(count)d of your project id fields has a problem: "
-                "There is no prefix or the prefix format is not recognised."
-            )
-            % {"count": len(invalid_project_id_paths)},
-            invalid_project_id_paths,
         )
 
 
@@ -148,6 +117,5 @@ def additional_checks():
     return [
         EmptyValueCheck(),
         CurrencyCheck(),
-        ProjectPrefixCheck(),
         OrgReferencesExistCheck(),
     ]
--- libcoveoc4ids/common_checks.py
+++ libcoveoc4ids/common_checks.py
@@ -4,6 +4,7 @@ from libcoveocds.lib.common_checks import get_releases_aggregates
 
 from libcoveoc4ids.additional_checks import additional_checks
 from libcoveoc4ids.config import LibCoveOC4IDSConfig
+from libcoveoc4ids.conformance_checks import conformance_checks
 
 
 def common_checks_oc4ids(context, upload_dir, json_data, schema_obj, lib_cove_oc4ids_config=None, cache=True):
@@ -17,10 +18,26 @@ def common_checks_oc4ids(context, upload_dir, json_data, schema_obj, lib_cove_oc
                                           'schema.json', context, cache=cache)
 
     context.update(common_checks["context"])
+
+    flattened_data = dict(flatten_dict(json_data))
+
     # end Common checks
 
+    # Conformance checks
+    conformance_checks_results = []
+
+    for conformance_check in conformance_checks():
+        check_result = conformance_check.process(json_data, flattened_data)
+        # Check passed, continue
+        if check_result is True:
+            continue
+
+        conformance_checks_results.append(check_result)
+
+    context.update({"conformance_checks": conformance_checks_results})
+    # end conformance checks
+
     # Additional checks
-    flattened_data = dict(flatten_dict(json_data))
 
     for additional_check in additional_checks():
         check_result = additional_check.process(json_data, flattened_data)
