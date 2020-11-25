6b19040b74f8bc43d5c5c5f9ae3d28406f0fc004
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-11-25 14:16:59 -0500

    setup: Add Python versions, Operating Systems to classifiers

--- setup.py
+++ setup.py
@@ -34,7 +34,12 @@ setup(
     },
     classifiers=[
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     entry_points={
         'console_scripts': [
