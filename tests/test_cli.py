c318658787f9a5ea7a38e1fe9eefe1341e861453
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-01-29 18:24:24 -0500

    test: Skip module on macos-pypy39

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,6 +6,12 @@ import pytest
 
 import tests.utils as utils
 
+# https://github.com/pypy/pypy/issues/4009
+pytestmark = pytest.mark.skipif(
+    platform.system() == "Darwin" and platform.python_implementation() == "PyPy",
+    reason="CI outputs .../lib/pypy3.9/site-packages/certifi/cacert.pem None",
+)
+
 
 def test_cli_runs():
     """ Test the cli tool runs as expected """
@@ -26,11 +32,6 @@ def test_compact():
     assert output[:21] == b'{"file_type":"json","'
 
 
-# https://github.com/pypy/pypy/issues/4009
-@pytest.mark.skipif(
-    platform.system() == "Darwin" and platform.python_implementation() == "PyPy",
-    reason="CI outputs .../lib/pypy3.9/site-packages/certifi/cacert.pem None",
-)
 def test_output(tmpdir):
     fixture_path = utils.get_path_for_fixture("example-data.json")
     output_path = tmpdir.join("result.json")
